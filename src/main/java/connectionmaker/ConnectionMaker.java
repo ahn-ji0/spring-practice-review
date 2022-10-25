aa54ea9c71a0a9e952a1d60b5c32b0d90a056ab7
Jiyoung <ahn.jiyoung15@gmail.com>
2022-10-25 11:28:20 +0900

    interface 적용

---
+++ src/main/java/connectionmaker/ConnectionMaker.java
@@ -0,0 +1,8 @@
+package connectionmaker;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface ConnectionMaker {
+    public Connection getConnection() throws SQLException;
+}
