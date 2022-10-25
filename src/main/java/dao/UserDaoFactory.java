6bf915681c183f57a1b68301691c6069682f4c02
Jiyoung <ahn.jiyoung15@gmail.com>
2022-10-25 13:34:18 +0900

    data source 적용

--- src/main/java/dao/UserDaoFactory.java
+++ src/main/java/dao/UserDaoFactory.java
@@ -3,16 +3,31 @@ package dao;
 import connectionmaker.AWSConnectionMaker;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.test.context.ContextConfiguration;
+
+import javax.sql.DataSource;
+import java.util.Map;
 
 @Configuration
 public class UserDaoFactory {
 
     @Bean
     public UserDao AWSUserDao(){
-        AWSConnectionMaker awsConnectionMaker = new AWSConnectionMaker();
-        UserDao userDao = new UserDao(awsConnectionMaker);
+        UserDao userDao = new UserDao(AWSDataSource());
 
         return userDao;
+    }
+
+    @Bean
+    public DataSource AWSDataSource(){
+        Map<String,String> env = System.getenv();
+
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass(com.mysql.cj.jdbc.Driver.class);
+        dataSource.setUrl(env.get("DB_HOST"));
+        dataSource.setUsername(env.get("DB_USER"));
+        dataSource.setPassword(env.get("DB_PASSWORD"));
+        return dataSource;
     }
 }
