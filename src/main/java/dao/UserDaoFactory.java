b88d66857b1b8beda8ddf6b3ca772b5ca492664b
Jiyoung <ahn.jiyoung15@gmail.com>
2022-10-25 13:16:19 +0900

    spring 적용

--- src/main/java/dao/UserDaoFactory.java
+++ src/main/java/dao/UserDaoFactory.java
@@ -1,9 +1,14 @@
 package dao;
 
 import connectionmaker.AWSConnectionMaker;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
 
+@Configuration
 public class UserDaoFactory {
 
+    @Bean
     public UserDao AWSUserDao(){
         AWSConnectionMaker awsConnectionMaker = new AWSConnectionMaker();
         UserDao userDao = new UserDao(awsConnectionMaker);
